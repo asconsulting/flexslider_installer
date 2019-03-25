3219ae9521f71973907ffe61f48200cd8e6c5f52
asconsulting <andrew@andrewstevens.consulting>
2019-03-25 00:59:24 -0400

    Different Method
    Use RunOnce instead of a console hook

---
+++ system/modules/flexslider_installer/config/install.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Flex Slider Installer for Contao 4+
+ *
+ * Copyright (C) 2018 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/flexslider_installer
+ * @link       https://andrewstevens.consulting
+ */
+ 
+ 
+
+/**
+ * Instantiate controller
+ */
+$objInstaller = new \FlexSlider\Installer();
+$objInstaller->install();
